fe541526b583ebf1d646c9351816b4d17f28216e
Brian Simiyu <briansimiyuj@gmail.com>
2024-10-12 09:59:55 +0300

    Authenticate and retrieve OAuth token

---
+++ src/background.ts
@@ -0,0 +1,14 @@
+/// <reference types="chrome"/>
+
+chrome.runtime.onInstalled.addListener(() =>{
+
+    console.log('Extension installed')
+
+}) 
+
+const authenticatedX = async () =>{
+
+    const consumerKey = "GA6DX1iLXl7jgxPyrVb6UpmzB",
+          consumerSecret = "NraPRNTDO3aSzNNr8qIqhZ1rYQtS5fXXsl8T72vUim7o0Nk0jx"
+
+}

1e11b39718dc2d782ac346efda8775291b2b11d3
Brian Simiyu <briansimiyuj@gmail.com>
2024-10-12 10:36:59 +0300

    Block content by making API calls
    Block content by making API calls

--- src/background.ts
+++ src/background.ts
@@ -12,3 +12,52 @@ const authenticatedX = async () =>{
           consumerSecret = "NraPRNTDO3aSzNNr8qIqhZ1rYQtS5fXXsl8T72vUim7o0Nk0jx"
 
 }
+
+
+const blockAdultContent = async (accountID: string) =>{
+
+    try{
+
+        const URL = `https://api.twitter.com/2/users/${accountID}/blocked`,
+              token = await authenticatedX(), 
+
+            response = await fetch(
+
+                URL,
+
+                {
+                    method: "POST",
+
+                    headers:{
+                        "Authorization": `Bearer ${token}`,
+                        "Content-Type": "application/json"
+                    }
+                }
+            
+            )
+
+        if(!response.ok){
+
+            const errorMessage = await response.json()
+
+            console.log(`Error blocking account: ${errorMessage}`)
+
+            throw new Error(errorMessage.detail || 'Failed to block account')
+
+        }
+
+        const data = await response.json()
+
+        console.log(`Blocked account: ${accountID}`, data)
+
+    }catch(error){
+
+        console.error(`Error blocking account: ${accountID}`, error)
+
+        throw error
+
+    }
+
+}
+
+export { authenticatedX }
